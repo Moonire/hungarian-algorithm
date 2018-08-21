50b7eddd1e64319322d60455eecd33b648826dd7
Mounir Mallek <mallekmounir@gmail.com>
2018-08-21 13:11:15 +0100

    Add __init__.py

---
+++ __init__.py
@@ -0,0 +1,11 @@
+from .hungarian import *
+
+
+__version_info__ = ("1", "1", "1", "dev")
+__date__ = "21 Aug 2017"
+
+__version__ = ".".join(__version_info__)
+__author__ = "Thom Dedecko"
+__url__ = "http://github.com/tdedecko/hungarian-algorithm"
+__copyright__ = "(c) 2010 Thom Dedecko"
+__license__ = "MIT License"
