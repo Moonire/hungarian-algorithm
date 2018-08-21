daa9715c44e7c51f537ee870d0792a60d7fb67b2
Mounir Mallek <mallekmounir@gmail.com>
2018-08-21 15:28:44 +0100

    typo

--- hungarian_algorithm/__init__.py
+++ hungarian_algorithm/__init__.py
@@ -5,7 +5,7 @@ __version_info__ = ("1", "1", "1", "dev")
 __date__ = "21 Aug 2017"
 
 __version__ = ".".join(__version_info__)
-__author__ = "Thom Dedecko, Markus Padourek, Marina Kollmitz,Federico Baldassarre,Mounir Mallek"
+__author__ = "Thom Dedecko, Markus Padourek, Marina Kollmitz, Federico Baldassarre, Mounir Mallek"
 __url__ = "http://github.com/tdedecko/hungarian-algorithm"
 __copyright__ = "(c) 2010 Thom Dedecko"
 __license__ = "MIT License"
