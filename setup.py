514ff27ff785537f14ba7a822124f12435ab80d8
Mounir Mallek <mallekmounir@gmail.com>
2018-08-21 14:25:18 +0100

    Modify setup.py

--- setup.py
+++ setup.py
@@ -3,10 +3,10 @@ from setuptools import setup
 import sys
 
 if sys.version_info[0] < 3:
-    raise Exception('markov_clustering requires Python 3')
+    raise Exception('Hungarian algorithm requires Python 3')
 
 distmeta = {}
-for line in open(os.path.join('markov_clustering', '__init__.py')):
+for line in open(os.path.join('hungarian algorithm', '__init__.py')):
     try:
         field, value = (x.strip() for x in line.split('='))
     except ValueError:
@@ -18,38 +18,26 @@ for line in open(os.path.join('markov_clustering', '__init__.py')):
         value = value.strip('\'"')
     distmeta[field] = value
     
-long_description = "See {}".format(distmeta["__homepage__"])
+long_description = "See {}".format(distmeta["__url__"])
 
 setup(
     name="markov_clustering",
     version=distmeta["__version_info__"],
-    description="Implementation of the Markov clustering (MCL) algorithm in python.",
+    description="Implementation of the Hungarian (Munkres) algorithm in python.",
     long_description=long_description,
     author=distmeta["__author__"],
-    author_email=distmeta["__contact__"],
-    url=distmeta["__homepage__"],
+    url=distmeta["__url__"],
     license="MIT",
-    platforms=["linux"],
-    packages=["markov_clustering"],
+    packages=["hungarian_algorithm"],
     python_requires='~=3.0',
-    install_requires=[
-        "numpy",
-        "scipy",
-        "scikit-learn",
-    ],
-    extras_require={
-        "drawing": ["networkx", "matplotlib"]
-    },
-    entry_points={
-    },
+    install_requires=["numpy"],
     classifiers=[
-        "Development Status :: 3 - Alpha",
+        "Development Status :: 1 - Alpha",
         "Intended Audience :: Developers",
         "Intended Audience :: Science/Research",
-        "Operating System :: POSIX :: Linux",
         "Programming Language :: Python",
         "Topic :: Scientific/Engineering",
         "License :: OSI Approved :: MIT License",
     ],
-    keywords = "bioinformatics clustering"
+    keywords = "hungarian algorithm"
 )
